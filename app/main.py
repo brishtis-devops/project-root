6bdebe2fb32ef3c13cf1064f94931dbe27b96ffe
Brishti Saha <brishtis154@gmail.com>
2026-04-05 20:53:43 +0530

    Update print statement for application run message

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
+
 if __name__ == "__main__":
-    print("Running Application 🚀")
+    print("Running App 🚀")
     print("Add:", add(5, 3))
     print("Subtract:", subtract(5, 3))
